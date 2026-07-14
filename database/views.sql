078896759133e30f91afa9852ba5276223ea6af8
morteza16907-ctrl <morteza16907@gmail.com>
2026-07-15 00:45:54 +0330

    Implement profit and tax calculation functions
    Added functions to calculate labor profit, accessory profit, tax amount, and customer club points.

--- database/views.sql
+++ database/views.sql
@@ -1,1 +1,101 @@
+----------------------------------------------------------
+-- CALCULATE LABOR PROFIT
+----------------------------------------------------------
 
+CREATE OR REPLACE FUNCTION calculate_labor_profit(
+
+purchase_percent NUMERIC,
+selling_percent NUMERIC,
+gold_amount NUMERIC
+
+)
+
+RETURNS NUMERIC
+
+AS
+$$
+
+BEGIN
+
+RETURN
+
+((selling_percent - purchase_percent) / 100) * gold_amount;
+
+END;
+
+$$
+LANGUAGE plpgsql IMMUTABLE;
+
+----------------------------------------------------------
+-- CALCULATE ACCESSORY PROFIT
+----------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION calculate_accessory_profit(
+
+purchase_price NUMERIC,
+selling_price NUMERIC
+
+)
+
+RETURNS NUMERIC
+
+AS
+$$
+
+BEGIN
+
+RETURN selling_price - purchase_price;
+
+END;
+
+$$
+LANGUAGE plpgsql IMMUTABLE;
+
+----------------------------------------------------------
+-- CALCULATE TAX AMOUNT
+----------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION calculate_tax_amount(
+
+base_amount NUMERIC,
+tax_percent NUMERIC
+
+)
+
+RETURNS NUMERIC
+
+AS
+$$
+
+BEGIN
+
+RETURN (base_amount * tax_percent) / 100;
+
+END;
+
+$$
+LANGUAGE plpgsql IMMUTABLE;
+
+----------------------------------------------------------
+-- CALCULATE CUSTOMER CLUB POINTS
+----------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION calculate_customer_points(
+
+invoice_amount NUMERIC
+
+)
+
+RETURNS NUMERIC
+
+AS
+$$
+
+BEGIN
+
+RETURN FLOOR(invoice_amount / 100000);
+
+END;
+
+$$
+LANGUAGE plpgsql IMMUTABLE;
